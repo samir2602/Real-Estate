6280c77038bbdf44a0d0c37b4d9eb145f55e6d31
Samir <120911119+samir2602@users.noreply.github.com>
2025-07-31 18:12:38 +0530

    typo fix

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -1,6 +1,6 @@
 <nav class="sidebar">
     <div class="sidebar-header">
-        <a href="#" class="sidebar-brand">
+        <a href="{{ route('/') }}" class="sidebar-brand">
           Real<span>Estate</span>
         </a>
         <div class="sidebar-toggler not-active">
